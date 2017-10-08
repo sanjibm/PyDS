8798b0db63a8e18bc9435299bc11c4b9d4aa0608
Sanjib Mitra <sanjibm2006@gmail.com>
2017-10-08 23:36:15 +0100

    binary search

---
+++ binary-search.py
@@ -0,0 +1,25 @@
+import math
+
+
+arr = [56,45,23,5,2,76,45,12,16,78,9,14,28,52,95]
+arr.sort()
+
+toSearch = int(raw_input('Enter a number: '))
+
+def binarySort(s, a):
+    start = 0
+    end = len(a) - 1
+    for i in range(start, end):
+        mid = int((start + end)/2)
+
+        if s == a[mid]:
+            return mid
+        if s < a[mid]:
+            end = mid - 1
+            continue
+        if s > a[mid]:
+            start = mid + 1
+            continue
+
+print arr
+print binarySort(toSearch, arr)
