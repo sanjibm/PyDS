436a0cec940b86bb7b134fe78cc09e8475108673
Sanjib Mitra <sanjibm2006@gmail.com>
2017-12-05 23:29:57 +0000

    performant two-sum

---
+++ two-sum.py
@@ -0,0 +1,33 @@
+"""
+Problem Statement
+=================
+Given an array of integers, return indices of the two numbers such that they add
+up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not
+use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+"""
+
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+
+    lookup = {}
+    for i, num in enumerate(nums):
+        if target - num in lookup:
+            return [lookup[target - num], i]
+        lookup[num] = i
+        print lookup
+    return []
+
+a = [2,3,5,6]
+print twoSum(a,8)
